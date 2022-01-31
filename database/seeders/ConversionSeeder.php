eeb2900dd583ccc19b4d422c77e753459a2f778d
Areg Sarkissian <aregsar@gmail.com>
2022-01-31 13:40:47 -0800

    update

--- database/seeders/ConversionSeeder.php
+++ database/seeders/ConversionSeeder.php
@@ -15,6 +15,11 @@ class ConversionSeeder extends Seeder
                 "amount" => 2
             ]);
 
+        // you can use the factory directly instaed of using it through the model Conversion::factory()
+        // \Acme\Converter\Database\Factories\ConversionFactory::new()->create([
+        //     "currency" => "EUR",
+        //     "amount" => 2
+        // ]);
 
         Conversion::create(["currency" => "GBP", "amount" => 3]);
     }
