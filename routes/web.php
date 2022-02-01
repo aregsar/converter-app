c91e5fa4c55b3d302f68e482c551a54bfec77853
Areg Sarkissian <aregsar@gmail.com>
2022-02-01 11:41:52 -0800

    update

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,64 @@ Route::get('/', function () {
 Route::get("test", function () {
     return config("acme-converter.packagename_prefix");
 });
+
+Route::get('/user', function () {
+
+    $user = new \App\Models\User(
+        [
+            "name" => "testuser",
+            "email" => "testuser@gmail.com",
+            "email_verified_at" => now(),
+            "password" => bcrypt("password"),
+            //'password' => \Illuminate\Support\Facades\Hash::make('password'),
+            "remember_token" => "abcd",
+        ]
+    );
+
+    $user->save();
+
+    return $user;
+});
+
+
+Route::get('/users', function () {
+
+    return \App\Models\User::all();
+});
+
+
+Route::get('/note', function () {
+
+    $user = \App\Models\User::all()->firstOrFail();
+
+    $user->notes()->create([
+        'content'  => 'All work and no play make Jack a dull boy',
+    ]);
+
+    return \Aregsar\Converter\Models\Note::all();
+});
+
+
+Route::get('/clearusers', function () {
+
+    foreach (\Aregsar\Converter\Models\Note::all() as $note) {
+        $note->delete();
+    }
+
+    foreach (\App\Models\User::all() as $user) {
+        $user->delete();
+    }
+
+    return \App\Models\User::all();
+});
+
+
+
+Route::get('/clearnotes', function () {
+
+    foreach (\Aregsar\Converter\Models\Note::all() as $note) {
+        $note->delete();
+    }
+
+    return \Aregsar\Converter\Models\Note::all();
+});
